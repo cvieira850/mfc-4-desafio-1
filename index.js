caf32a0f5758743711919e10af5dc326fa616855
CAIO VIEIRA <caio@dataclick.com.br>
2020-08-25 14:02:44 -0300

    feat: docker and index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 const express = require("express");
 
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
 const app = express();
 app.get("/", (req, res) => {
   res.send("Eu sou Full Cycle");
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://8080:0.0.0.0}`);
+console.log(`Running on http://${HOST}: ${PORT}`);
